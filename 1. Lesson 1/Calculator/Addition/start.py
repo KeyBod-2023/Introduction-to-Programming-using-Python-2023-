4e3fe02cba7c74948ada87fdaddac5316b9f9e5f
Vusani Makhomu <vusani.makhomu@soliditech.com>
2023-12-07 19:39:56 +0200

    Completed addition feature

---
+++ 1. Lesson 1/Calculator/Addition/start.py
@@ -0,0 +1,6 @@
+stringNumberOne = input('Enter the first number: ')
+stringNumberTwo = input("Enter the second number: ")
+
+intNumberOne = int(stringNumberOne)
+intNumberTwo = int(stringNumberTwo)
+print(intNumberOne + intNumberTwo)
